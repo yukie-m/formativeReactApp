e54d578933445ebd87c8af77c3c8905ec736fd13
yukie-m <270141737@yoobeestudent.ac.nz>
2022-03-10 09:27:30 +1300

    login start build

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Artists from "./Components/Artists";
+import Login from "./Components/Login";
 
 function App() {
   return (
     <div className="App">
       <Artists />
       <p>Hello, this is a github workshop</p>
+      <Login />
     </div>
   );
 }
