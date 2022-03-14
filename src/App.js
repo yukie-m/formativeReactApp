a856a5b026f5efedb1ad40a7c9cc56603dbb5751
ryan-bakker <270031649@yoobeestudent.ac.nz>
2022-03-15 12:05:15 +1300

    checkout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Products from "./Components/Products";
 import Login from "./Components/Login";
 import { Routes, Route, Link } from "react-router-dom";
+import Checkout from "./Components/Checkout";
 
 function App() {
   return (
@@ -10,9 +11,12 @@ function App() {
       <Link to="/">Products</Link>
       <br />
       <Link to="/login">User Login</Link>
+      <br />
+      <Link to="/checkout">User Checkout</Link>
       <Routes>
         <Route path="/" element={<Products />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
       <Products />
     </div>
