8b7b8b39b39b07995a518aa162108d849d3e765b
yukie-m <270141737@yoobeestudent.ac.nz>
2022-03-15 11:39:49 +1300

    added create item page

--- src/Components/ListItem.jsx
+++ src/Components/ListItem.jsx
@@ -68,34 +68,70 @@ function ListItem() {
       <h1>List Your Item</h1>
 
       <form onSubmit={onSubmit} style={formStyle}>
-        <input type="text" placeholder="Your Name" ref={sellerRef} required />
-        <input type="text" placeholder="Location" ref={areaRef} required />
-        <input type="text" placeholder="Item Name" ref={nameRef} required />
-        <input
-          type="text"
-          placeholder="Collecting Way"
-          ref={collectingRef}
-          required
-        />
-        <input type="number" placeholder="Price" ref={priceRef} required />
-        <input type="text" placeholder="item" ref={itemRef} required />
-        <input type="text" placeholder="Category" ref={categoryRef} required />
-        <input type="text" placeholder="Size" ref={sizeRef} required />
-        <input
-          type="text"
-          placeholder="Condition"
-          ref={conditionRef}
-          required
-        />
-        <input type="text" placeholder="Image" ref={thumbRef} required />
-        <textarea
-          name="w3review"
-          rows="4"
-          cols="50"
-          placeholder="Description"
-          ref={descriptionRef}
-          required
-        />
+        <p>
+          Seller:{" "}
+          <input type="text" placeholder="Your Name" ref={sellerRef} required />
+        </p>
+        <p>
+          Location:{" "}
+          <input type="text" placeholder="Location" ref={areaRef} required />
+        </p>
+        <p>
+          Item Name:{" "}
+          <input type="text" placeholder="Item Name" ref={nameRef} required />
+        </p>
+        <p>
+          Delivery:{" "}
+          <input
+            type="text"
+            placeholder="Collecting Way"
+            ref={collectingRef}
+            required
+          />
+        </p>
+        <p>
+          Price:{" "}
+          <input type="number" placeholder="Price" ref={priceRef} required />
+        </p>
+        <p>
+          Item: <input type="text" placeholder="item" ref={itemRef} required />
+        </p>
+        <p>
+          Category:{" "}
+          <input
+            type="text"
+            placeholder="Category"
+            ref={categoryRef}
+            required
+          />
+        </p>
+        <p>
+          Size: <input type="text" placeholder="Size" ref={sizeRef} required />
+        </p>
+        <p>
+          Condition:{" "}
+          <input
+            type="text"
+            placeholder="Condition"
+            ref={conditionRef}
+            required
+          />
+        </p>
+        <p>
+          Image:{" "}
+          <input type="text" placeholder="Image" ref={thumbRef} required />
+        </p>
+        <p>
+          Description:{" "}
+          <textarea
+            name="w3review"
+            rows="4"
+            cols="50"
+            placeholder="Description"
+            ref={descriptionRef}
+            required
+          />
+        </p>
         <button onClick={openModal} className="create-btn">
           SUBMIT
         </button>
