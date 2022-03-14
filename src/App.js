62ff4c0265f1add831cdc0d155d5f41cd430db9b
ryan-bakker <270031649@yoobeestudent.ac.nz>
2022-03-15 10:07:31 +1300

    router start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from "react";
 import "./App.css";
 import Products from "./Components/Products";
+import Login from "./Components/Login";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Link to="/">Products</Link>
+      <br />
+      <Link to="/login">User Login</Link>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <Products />
     </div>
   );
